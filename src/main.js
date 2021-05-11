50382d26143295fcc3348e26e432cdbe906f14f5
Zhendong Jiang <zjiang40@ucsc.edu>
2021-05-11 14:17:56 -0700

    initial setting

---
+++ src/main.js
@@ -0,0 +1,29 @@
+/* 
+** Name: Zhendong Jiang - programming, game design
+**       Nathan Pon - art, audio assets
+**       Jiahui Li - art, audio assets
+** Porject: Final Game:
+** Game Title: 
+** Date: 
+*/
+
+// game configuration
+let config = {
+    type: Phaser.CANVAS,
+    width: 640,
+    height: 480,
+    scale: {
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            // debug: true,
+            gravity: {
+                x: 0,
+                y: 0
+            }
+        }
+    },
+    scene: [ Load, Menu, Credits, Play, End ]
+}
---
+++ src/scenes/Play.js
@@ -0,0 +1,21 @@
+/* 
+** Name: Zhendong Jiang - programming, game design
+**       Nathan Pon - art, audio assets
+**       Jiahui Li - art, audio assets
+** Porject: Final Game:
+** Game Title: 
+** Date: 
+*/
+
+class Play extends Phaser.Scene {
+    constructor() {
+        super("playScene");
+    }
+
+    create() {
+        
+    }
+
+    update() {
+    }
+}
