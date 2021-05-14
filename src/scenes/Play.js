a4462650cd91b0ab6bb8cc4d1d8b64e98cfa9a8a
Nathan Pon <nathanpon98@gmail.com>
2021-05-14 10:56:13 -0700

    shooting Mechanic

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -23,32 +23,37 @@ class Play extends Phaser.Scene {
       
 
         this.add.image(0, 0, 'city').setOrigin(0, 0);
-        this.p1 = new Player (this, game.config.width/2, game.config.height-borderUIsize-borderPadding,'crosshair').setOrigin(0.5,0); //add player 1
-    
+        this.p1 = new Player (this, game.config.width/2, game.config.height-borderUIsize-borderPadding,'crosshair').setOrigin(0.5,0.5); //add player 1
+       
       //Player movement with mouse 
       this.p1movement();
       
     }
-    
+
     p1movement() {
       this.input.on('pointermove', (pointer) =>{ 
       this.p1.x = pointer.x;
       this.p1.y = pointer.y;
       })
 
-      //Play SFX on click
+      //Fire projectile on click
       this.input.on('pointerdown', (pointer) =>{ 
+       let bullet = this.physics.add.sprite(this.p1.x, this.p1.y+300,'projectile');
+       bullet.body.velocity.y = -300;
         this.sfx = this.sound.add('gunshot', {
           mute: false,
           volume: 1,
           rate: 1,
           loop: false 
       });
-        this.sfx.play();
+        this.sfx.play();   
       })
 
     }
 
+  
     update() {
+    
+      
     }
 }
