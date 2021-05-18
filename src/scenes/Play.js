b3961501f1360228cc52d2cb72d09ada26712319
Nathan Pon <nathanpon98@gmail.com>
2021-05-18 12:43:18 -0700

    Ammo Count on HUD

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -12,36 +12,46 @@ class Play extends Phaser.Scene {
         super("playScene");
     }
 
-    create() {
-        //this.add.rectangle(0,borderUIsize + borderPadding, game.config.width, borderUIsize * 2, 283033 ).setOrigin(0,0); //green UI
-    
- 
-        this.add.rectangle(0,0, game.config.width, borderUIsize * 2, 0xFFFFFF).setOrigin(0,0);
-        this.add.rectangle(0,0,borderUIsize, game.config.height - borderUIsize,0xFFFFFF ).setOrigin(0,0);
-        this.add.rectangle(0,0,borderUIsize, game.config.height,0xFFFFFF).setOrigin(0,0);
-        this.add.rectangle(game.config.width - borderUIsize, 0, borderUIsize, game.config.height, 0xFFFFFF).setOrigin(0,0);
-      
 
+    create() {
+     
         this.add.image(0, 0, 'city').setOrigin(0, 0);
-        this.p1 = new Player (this, game.config.width/2, game.config.height-borderUIsize-borderPadding,'crosshair').setOrigin(0.5,0.5); //add player 1
-       
+         this.p1Ammo = 50;
+      
+        //UI
+        this.add.rectangle(0,borderUIsize + borderPadding, game.config.width/4, borderUIsize * 2, 283033 ).setOrigin(0,0.7); //green UI
+        this.healthText = this.add.text(16,16, 'Health: ', { fontSize: '16px', fill: '#000' });
+        this.add.rectangle(0,borderUIsize + borderPadding, game.config.width/4, borderUIsize * 2, 283033 ).setOrigin(-4,0.7); 
+        this.ammoText = this.add.text(16,32,`Ammo: ${this.p1Ammo}`, { fontSize: '16px', fill: '#000' });
+        
+        this.m1 = new Enemy(this, game.config.width/2, game.config.height-borderUIsize-borderPadding).setOrigin(0.5,1);
+        this.m2 = new Enemy(this, game.config.width/2, game.config.height-borderUIsize-borderPadding).setOrigin(-0.5,1);
+        this.p1 = new Player (this, game.config.width/2, game.config.height-borderUIsize-borderPadding).setOrigin(0.5,0.5); //add player 1
+
       //Player movement with mouse 
-      this.p1movement();
+      this.p1Controls();
+     // this.physics.add.overlap(bullet,Enemy,this.hitEnemy, null, this)
+      
       
     }
 
-    p1movement() {
+ 
+    p1Controls() {
+     
       this.input.on('pointermove', (pointer) =>{ 
       this.p1.x = pointer.x;
       this.p1.y = pointer.y;
       })
 
       //Fire projectile on click
-      this.input.on('pointerdown', (pointer) =>{ 
-      this.bullet = this.physics.add.sprite(this.p1.x, this.p1.y+300,'projectile'); //Working code
-    //let bullet = new projectile (this, this.p1.x, this.p1.y, 'projectile');
-       this.bullet.body.velocity.y = -300;
-        this.sfx = this.sound.add('gunshot', {
+     this.input.on('pointerdown', (pointer) =>{ 
+     this.bullet = this.physics.add.sprite(this.p1.x, this.p1.y+300,'projectile'); //Working code
+     this.bullet.body.velocity.y = -300;  //Makes projectile visible
+   
+     this.p1Ammo -= 1
+     this.ammoText.text = `Ammo: ${this.p1Ammo} `;
+     
+      this.sfx = this.sound.add('gunshot', {
           mute: false,
           volume: 1,
           rate: 1,
@@ -53,9 +63,4 @@ class Play extends Phaser.Scene {
     }
 
 
-  
-    update() {
-    
-      
-    }
 }
