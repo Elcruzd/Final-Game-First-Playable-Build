56a08afa9e12f23adccd2c02bfd39ad9452185a3
Nathan Pon <nathanpon98@gmail.com>
2021-05-23 21:19:03 -0700

    Particle FX

--- src/scenes/Load.js
+++ src/scenes/Load.js
@@ -20,12 +20,23 @@ class Load extends Phaser.Scene {
         this.load.image ('crosshair','CHgame.png')
         this.load.image ('monster', 'monsterBoss.png')
         this.load.image ('projectile', 'bullet.png')
+
+        //Different particle effects
         this.load.image ('orange', 'particle.png')
         this.load.image ('particle2', 'particle2.png')
+        this.load.image ('explode', 'particle3.png')
+
+        /*BOSS SPRITE TO BE ADDED
+        this.load.spritesheet ('boss', 'bossSprites.png',{
+        frameWidth:452,
+        frameHeight:570 
+        });*/
+    
         // load audio asset
         this.load.audio('gunshot', 'gunshot.wav');
         this.load.audio('select', 'select.wav');
         this.load.audio('monsterHit', 'monsterHit.wav');
+        this.load.audio ('enemyDefeat', 'enemyDefeat.wav');
         this.load.audio('bgm', 'bgm.wav');
     
 
